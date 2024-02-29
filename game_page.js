cb40c9aa5048f9b2514e0ca4613f8c7fa8b7bb73
andrezinhogameplays <137837130+andrezinhogameplays@users.noreply.github.com>
2024-02-29 20:28:35 -0300

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,17 @@
+function send() {
+    get_word = document.getElementById(”word”).value;
+    word = get_word.toLowerCase ();     
+console.log(”word in  lowerCase = + word);
+
+charAt1 = word.charAt(1);
+ console.log(charAt1);
+
+ lenght_divide_2 = math.floor(word.lenght/2);
+ charAt2 = word.charAt(lenght_divine_2);
+ console.log(charAt2);
+
+ lenght_minus_1 = word.lenght - 1;      
+ charAt3 = word.charAt(lenght_minus_1);
+ console.log(charAt3);      
+ 
+ remove_charAt1 = word.replace(charAt1, )
